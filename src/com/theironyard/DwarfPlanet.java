ece64e232f1d7cd58d4d832be1b46823afae6b0d
Krista Blachian <krista.blachian@scifusion360.com>
2018-12-04 16:14:02 -0500

    subclassed HeavenlyBodies

---
+++ src/com/theironyard/DwarfPlanet.java
@@ -0,0 +1,8 @@
+package com.theironyard;
+
+public class DwarfPlanet extends HeavenlyBody {
+
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+}
--- src/com/theironyard/HeavenlyBody.java
+++ src/com/theironyard/HeavenlyBody.java
@@ -3,7 +3,7 @@ package com.theironyard;
 import java.util.HashSet;
 import java.util.Set;
 
-public final class HeavenlyBody {
+public abstract class HeavenlyBody {
     private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
@@ -63,7 +63,6 @@ public final class HeavenlyBody {
 
     @Override
     public final int hashCode() {
-        System.out.println("hashcode called");
         return this.name.hashCode() + 57 + this.bodyType.hashCode();
     }
 
