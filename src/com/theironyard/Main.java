8b31871b93fc87c2dd61fa59499ef04e32d9204b
Krista Blachian <krista.blachian@scifusion360.com>
2018-12-05 15:56:19 -0500

    exercise complete

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -108,8 +108,15 @@ public class Main {
         System.out.println(earth1.equals(earth2));
         System.out.println(earth2.equals(earth1));
 
+        solarSystem.put(pluto.getKey(), pluto);
+        System.out.println(solarSystem.get(HeavenlyBody.makeKey("Pluto", HeavenlyBody.BodyTypes.PLANET)));
+        System.out.println(solarSystem.get(HeavenlyBody.makeKey("Pluto", HeavenlyBody.BodyTypes.DWARF_PLANET)));
 
-
+        System.out.println();
+        System.out.println("The solar system contains:");
+        for(HeavenlyBody heavenlyBody : solarSystem.values()) {
+            System.out.println(heavenlyBody);
+        }
 
     }
 }
